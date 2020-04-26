2cf6efe4563dda9520c6df6e9dacb9460288d198
martin <martin.johnsson@roslin.ed.ac.uk>
2020-04-26 18:51:39 +0200

    Refactor to make it easier to add balancing selection; wrap in a loop

--- R/simple_simulation.R
+++ R/simple_simulation.R
@@ -10,122 +10,67 @@
 library(AlphaSimR)
 
 
-## Set up simulation
-
-founderpop <- runMacs(nInd = 3200,
-                      nChr = 10,
-                      segSites = 1000)
-
-
-simparam <- SimParam$new(founderpop)
-simparam$restrSegSites(maxQtl = 100,
-                       maxSnp = 900,
-                       overlap = FALSE)
-simparam$setGender("yes_sys")
-
-simparam$addTraitA(nQtlPerChr = 100)
-simparam$setVarE(h2 = 0.4)
-
-simparam$addSnpChip(nSnpPerChr = 900)
-
-founders <- newPop(founderpop,
-                   simParam = simparam)
-
-
-## Pick a lethal variant
-
-founder_geno <- pullSnpGeno(founderpop,
-                            simParam = simparam)
-f <- colSums(founder_geno)/(2 * nrow(founder_geno))
-candidates <- which(f > 0.04 & f < 0.06)
-
-if (length(candidates) < 1) {
-    stop("Found no candidate loci with appropriate frequency.")
-}
-
-lethal_ix <- sample(candidates, 1)
-other_snp_ix <- setdiff(candidates, lethal_ix)
-
-
-## Carrier test function
-
-carrier_test <- function(pop, lethal_ix) {
-
-    pullSnpGeno(pop,
-                simParam = simparam)[, lethal_ix]
-
-}
+source("R/simulation_functions.R")
 
 
 
-## Breeding simulation
+for (rep_ix in 1:10) {
 
-n_gen <- 40
+    ## Set up simulation
 
-generations <- list(n_gen)
+    setup <- make_simulation(n_ind = 3200,
+                             n_chr = 10,
+                             h2 = 0.4)
 
-generations[[1]] <- founders
+    founders <- setup$founderpop
+    simparam <- setup$simparam
 
-for (gen_ix in 2:n_gen) {
-    ## Take all females as dams and split them in carrier/noncarrier
 
-    dams <- generations[[gen_ix - 1]][generations[[gen_ix - 1]]@gender == "F"]
-    
-    dam_carrier_status <- carrier_test(dams,
-                                       lethal_ix)
+    ## Pick a lethal variant
 
-    carrier_dams <- dams[dam_carrier_status > 0]
-    noncarrier_dams <- dams[dam_carrier_status == 0]
+    chosen_lethal <- pick_lethal(founderpop,
+                             lethal_is = "snp",
+                             simparam)
+    lethal_ix <- chosen_lethal$lethal_ix
+    other_snp_ix <- chosen_lethal$other_snp_ix
 
-    n_carrier_dams <- sum(dam_carrier_status > 0)
 
-    ## Select the top sires and split out noncarriers
-    sires <- selectInd(pop = generations[[gen_ix - 1]],
-                       trait = 1,
-                       use = "pheno",
-                       gender = "M",
-                       nInd = 200,
-                       simParam = simparam)
+    ## Breeding simulation
 
-    sire_carrier_status <- carrier_test(sires,
-                                        lethal_ix)
-    
-    noncarrier_sires <- sires[sire_carrier_status == 0]
+    n_gen <- 40
 
-    ## Create matings for noncarrier/noncarrier and carrier/noncarrier separately
-    ## Splitting matings proportionally between the groups
-    
-    noncarrier_dam_x_any_sire <- randCross2(females = noncarrier_dams,
-                                            males = sires,
-                                            nProgeny = 1,
-                                            nCrosses = 2 * noncarrier_dams@nInd,
-                                            simParam = simparam)
+    generations <- list(n_gen)
 
-    if (n_carrier_dams > 0) {
-        carrier_dam_x_noncarrier_sire <- randCross2(females = carrier_dams,
-                                                    males = noncarrier_sires,
-                                                    nProgeny = 1,
-                                                    nCrosses = 2 * carrier_dams@nInd,
-                                                    simParam = simparam)
-    
-        generations[[gen_ix]] <- c(carrier_dam_x_noncarrier_sire,
-                                   noncarrier_dam_x_any_sire)
-    } else {
-        generations[[gen_ix]] <- noncarrier_dam_x_any_sire
+    generations[[1]] <- founders
+
+    for (gen_ix in 2:n_gen) {
+
+        generations[[gen_ix]] <-
+        breed_avoiding_carrier_x_carrier(generations[[gen_ix - 1]],
+                                         lethal_ix,
+                                         lethal_is = "snp",
+                                         simparam)
     }
+    
+
+    ## Create simulation object
+
+    simulation_results <- list(generations = generations,
+                               carrier_status = lapply(generations,
+                                                       carrier_test,
+                                                       lethal_ix = lethal_ix,
+                                                       lethal_is = "snp",
+                                                       simparam),
+                               simparam = simparam,
+                               lethal_ix = lethal_ix,
+                               other_snp_ix = other_snp_ix)
+    
+    saveRDS(simulation_results, 
+            file = paste("simulations/simple_simulations/results_",
+                         rep_ix,
+                         ".Rds",
+                         sep = ""))
 }
-
-
-## Create simulation object
-
-simulation_results <- list(generations = generations,
-                           carrier_status = lapply(generations,
-                                                   carrier_test,
-                                                   lethal_ix = lethal_ix),
-                           simparam = simparam,
-                           lethal_ix = lethal_ix,
-                           other_snp_ix = other_snp_ix)
-
-saveRDS(simulation_results, 
-        file = "simulations/simple_simulations/results.Rds")
-
+            
+    
+    

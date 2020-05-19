570267246a84e0154a51ad8e6c8d729eb63aaa88
martin <martin.johnsson@roslin.ed.ac.uk>
2020-05-19 14:07:32 +0200

    Typo

--- scripts/run_simple_simulations.sh
+++ scripts/run_simple_simulations.sh
@@ -8,7 +8,7 @@ if [ ! -d simulations/simple_simulations ]; then
     mkdir simulations/simple_simulations
 fi
 
-for REP in 1..100; do
+for REP in {1..100}; do
 
     Rscript R/simple_simulation.R $REP
     
--- scripts/run_simple_simulations_balancing.sh
+++ scripts/run_simple_simulations_balancing.sh
@@ -8,7 +8,7 @@ if [ ! -d simulations/simple_simulations_balancing ]; then
     mkdir simulations/simple_simulations_balancing
 fi
 
-for REP in 1..100; do
+for REP in {1..100}; do
 
     Rscript R/simple_simulation_balancing.R $REP
     

8917fe0ae27715a95bc6fe7fa10c81734fc87df8
martin <martin.johnsson@roslin.ed.ac.uk>
2020-08-20 11:40:38 +0200

    Run with exempt top sires

--- scripts/run_simple_simulations.sh
+++ scripts/run_simple_simulations.sh
@@ -8,7 +8,7 @@ if [ ! -d simulations/simple_simulations ]; then
     mkdir simulations/simple_simulations
 fi
 
-for SELECTION_RULE in avoid_carrier_x_carrier unknown_lethal selection_against; do
+for SELECTION_RULE in avoid_carrier_x_carrier unknown_lethal; do
 
     for LETHAL_IS in snp qtl; do
     
@@ -21,11 +21,36 @@ for SELECTION_RULE in avoid_carrier_x_carrier unknown_lethal selection_against; 
             Rscript R/simple_simulation.R \
                 $SELECTION_RULE \
                 $LETHAL_IS \
+                0 \
                 simulations/simple_simulations/${SELECTION_RULE}_${LETHAL_IS}/populations_$REP.Rds \
-                simulations/simple_simulations/${SELECTION_RULE}_${LETHAL_IS}/results_$REP.Rds \
+                simulations/simple_simulations/${SELECTION_RULE}_${LETHAL_IS}/results_$REP.Rds 
                 
         done
         
-    done        
+    done    
+    
+done
+
+
+SELECTION_RULE=selection_against
+
+for LETHAL_IS in snp qtl; do
+    
+    if [ ! -d simulations/simple_simulations/${SELECTION_RULE}_${LETHAL_IS}_${N_TOP_EXEMPT}exempt ]; then
+        mkdir simulations/simple_simulations/${SELECTION_RULE}_${LETHAL_IS}_${N_TOP_EXEMPT}exempt
+    fi
+
+    for REP in {1..100}; do
+    
+        for N_TOP_EXEMPT in 0 10 100; do
+
+            Rscript R/simple_simulation.R \
+                $SELECTION_RULE \
+                $LETHAL_IS \
+                $N_TOP_EXEMPT \
+                simulations/simple_simulations/${SELECTION_RULE}_${LETHAL_IS}_${N_TOP_EXEMPT}exempt/populations_$REP.Rds \
+                simulations/simple_simulations/${SELECTION_RULE}_${LETHAL_IS}_${N_TOP_EXEMPT}exempt/results_$REP.Rds 
+            
+    done
     
 done
